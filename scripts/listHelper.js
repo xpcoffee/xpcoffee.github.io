7aa372cc61797d44213085571483841f34ea1792
Rick Bosch <xpc.dev@gmail.com>
2015-05-17 21:51:02 +0200

    Added header. Generalized createULFromArray and added createOLFromArray.

--- scripts/listHelper.js
+++ scripts/listHelper.js
@@ -3,21 +3,48 @@
  * email: 	xpc.dev@gmail.com 
  */
 
-
 /*
- * Creates an unordered list from a given array and places it after a given element.
+ * Creates HTML unordered list from an array, places it after "previous element"
  */
 function createULFromArray(previousElement, array) {
-	var ul = document.createElement("ul");
+	createListFromArray(previousElement, array, "ul");
+}
+
+/*
+ * Creates HTML ordered list from an array, places it after "previous element"
+ */
+function createOLFromArray(previousElement, array) {
+	createListFromArray(previousElement, array, "ol");
+}
+
+/*
+ * Creates a list from a given array and places it after a given element.
+ */
+function createListFromArray(previousElement, array, type) {
+	var newList = getList(type);
 	for (i in array) {
 		var element = array[i];
 		var li = document.createElement("li");
 		if(Array.isArray(element)) {
-			createULFromArray(ul.lastChild, element);
+			createListFromArray(newList.lastChild, element, type);
 		} else {
 			li.innerHTML = element;
-			ul.appendChild(li);
+			newList.appendChild(li);
 		}
 	}
-	previousElement.parentNode.insertBefore(ul, previousElement.nextSibling);
+	previousElement.parentNode.insertBefore(newList, previousElement.nextSibling);
+}
+
+/*
+ * Returns HTML list of given type
+ */
+function getList(type) {
+	switch(type){
+		case "ul":
+			return document.createElement("ul");
+		case "ol":
+			return document.createElement("ol");
+		default:
+			return null;
+	}
 }
